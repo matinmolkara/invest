5119f97796a0c727e520096d76ee406d2b231f48
matin <97048003+matinmolkara@users.noreply.github.com>
2024-05-19 10:46:28 +0330

    second commit

--- src/context/DataContext.js
+++ src/context/DataContext.js
@@ -1,4 +1,4 @@
-import { useState,createContext,useContext} from "react";
+import { useState,createContext} from "react";
 import { pageTitle } from "./constants";
 
 const DataContext = createContext ();
@@ -6,7 +6,7 @@ const DataContext = createContext ();
 export const DataProvider = ({children}) =>{
     const [isActive, setIsActive] = useState(false);
   
-    const [data, setData] = useState({
+    const [data] = useState({
         about: { 
             title: 'All You Need To Know About invest pack!',
             content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.' },
